c0cfe2376836a17541a509a169317a570899e580
cscyiu <33767884+cscyiu@users.noreply.github.com>
2024-03-18 18:54:43 -0400

    Update chat-app.py

--- app/chat-app.py
+++ app/chat-app.py
@@ -4,14 +4,14 @@ import json
 import os
 
 st.title('IBM Data and AI: MDM and watsonx.ai Demo 🤖')
-st.caption("🚀 A chatbot powered by watsonx.ai")
+st.caption("🚀 A chatbot powered by watsonx.ai 🚀")
 
 with st.sidebar:
     watsonx_api_key = st.text_input("Watsonx API Key", key="watsonx_api_key", value=os.environ.get("API_KEY"), type="password")
     watsonx_url = st.text_input("Watsonx URL", key="watsonx_url", value="https://us-south.ml.cloud.ibm.com", type="default")
     #TODO: change this to a select box with more than one model
     watsonx_model = st.text_input("Model", key="watsonx_model", value="meta-llama/llama-2-70b-chat", type="default")
-    #watsonx_model = st.selectbox('watsonX Foundation Model:?', ('FLAN_T5_XL', 'FLAN_T5_XXL', 'FLAN_UL2', 'GPT_NEOX', 'GRANITE_13B_CHAT', 'GRANITE_13B_CHAT_V2', 'GRANITE_13B_INSTRUCT', 'GRANITE_13B_INSTRUCT_V2', 'LLAMA_2_13B_CHAT', 'LLAMA_2_70B_CHAT', 'MIXTRAL_8X7B_INSTRUCT_V01_Q', 'MPT_7B_INSTRUCT2', 'MT0_XXL', 'STARCODER', 'ELYZA_JAPANESE_LLAMA_2_7B_INSTRUCT')) 
+    #watsonx_model = st.selectbox("watsonx.ai Foundation Model:?", ("FLAN_T5_XL", 'FLAN_T5_XXL', 'FLAN_UL2', 'GPT_NEOX', 'GRANITE_13B_CHAT', 'GRANITE_13B_CHAT_V2', 'GRANITE_13B_INSTRUCT', 'GRANITE_13B_INSTRUCT_V2', 'LLAMA_2_13B_CHAT', 'LLAMA_2_70B_CHAT', 'MIXTRAL_8X7B_INSTRUCT_V01_Q', 'MPT_7B_INSTRUCT2', 'MT0_XXL', 'STARCODER', 'ELYZA_JAPANESE_LLAMA_2_7B_INSTRUCT')) 
 
     watsonx_project_id = st.text_input("Watsonx Project ID", key="watsonx_project_id", value=os.environ.get("Project_ID"), type="default")
     watsonx_model_params = st.text_input("Params", key="watsonx_model_params", value='{"decoding_method":"sample", "max_new_tokens":200, "temperature":0.5}', type="default" )
